fef82cde806dd3b77aded016a60e8394ac333d53
Büşra KOYUNCU <100695152+busra159@users.noreply.github.com>
2022-04-08 22:52:49 +0300

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -1,53 +1,173 @@
-import math
+'''
+    File name         : tracker.py
+    File Description  : Tracker Using Kalman Filter & Hungarian Algorithm
+    Author            : Srini Ananthakrishnan
+    Date created      : 07/14/2017
+    Date last modified: 07/16/2017
+    Python Version    : 2.7
+'''
+
+# Import python libraries
+import numpy as np
+from kalman_filter import KalmanFilter
+from common import dprint
+from scipy.optimize import linear_sum_assignment
 
 
-class EuclideanDistTracker:
-    def __init__(self):
-        # Store the center positions of the objects
-        self.center_points = {}
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
-        self.id_count = 0
+class Track(object):
+    """Track class for every object to be tracked
+    Attributes:
+        None
+    """
+
+    def __init__(self, prediction, trackIdCount):
+        """Initialize variables used by Track class
+        Args:
+            prediction: predicted centroids of object to be tracked
+            trackIdCount: identification of each track object
+        Return:
+            None
+        """
+        self.track_id = trackIdCount  # identification of each track object
+        self.KF = KalmanFilter()  # KF instance to track this object
+        self.prediction = np.asarray(prediction)  # predicted centroids (x,y)
+        self.skipped_frames = 0  # number of frames skipped undetected
+        self.trace = []  # trace path
 
 
-    def update(self, objects_rect):
-        # Objects boxes and ids
-        objects_bbs_ids = []
+class Tracker(object):
+    """Tracker class that updates track vectors of object tracked
+    Attributes:
+        None
+    """
 
-        # Get center point of new object
-        for rect in objects_rect:
-            x, y, w, h = rect
-            cx = (x + x + w) // 2
-            cy = (y + y + h) // 2
+    def __init__(self, dist_thresh, max_frames_to_skip, max_trace_length,
+                 trackIdCount):
+        """Initialize variable used by Tracker class
+        Args:
+            dist_thresh: distance threshold. When exceeds the threshold,
+                         track will be deleted and new track is created
+            max_frames_to_skip: maximum allowed frames to be skipped for
+                                the track object undetected
+            max_trace_lenght: trace path history length
+            trackIdCount: identification of each track object
+        Return:
+            None
+        """
+        self.dist_thresh = dist_thresh
+        self.max_frames_to_skip = max_frames_to_skip
+        self.max_trace_length = max_trace_length
+        self.tracks = []
+        self.trackIdCount = trackIdCount
 
-            # Find out if that object was detected already
-            same_object_detected = False
-            for id, pt in self.center_points.items():
-                dist = math.hypot(cx - pt[0], cy - pt[1])
+    def Update(self, detections):
+        """Update tracks vector using following steps:
+            - Create tracks if no tracks vector found
+            - Calculate cost using sum of square distance
+              between predicted vs detected centroids
+            - Using Hungarian Algorithm assign the correct
+              detected measurements to predicted tracks
+              https://en.wikipedia.org/wiki/Hungarian_algorithm
+            - Identify tracks with no assignment, if any
+            - If tracks are not detected for long time, remove them
+            - Now look for un_assigned detects
+            - Start new tracks
+            - Update KalmanFilter state, lastResults and tracks trace
+        Args:
+            detections: detected centroids of object to be tracked
+        Return:
+            None
+        """
 
-                if dist < 25:
-                    self.center_points[id] = (cx, cy)
-                    print(self.center_points)
-                    objects_bbs_ids.append([x, y, w, h, id])
-                    same_object_detected = True
-                    break
+        # Create tracks if no tracks vector found
+        if (len(self.tracks) == 0):
+            for i in range(len(detections)):
+                track = Track(detections[i], self.trackIdCount)
+                self.trackIdCount += 1
+                self.tracks.append(track)
 
-            # New object is detected we assign the ID to that object
-            if same_object_detected is False:
-                self.center_points[self.id_count] = (cx, cy)
-                objects_bbs_ids.append([x, y, w, h, self.id_count])
-                self.id_count += 1
+        # Calculate cost using sum of square distance between
+        # predicted vs detected centroids
+        N = len(self.tracks)
+        M = len(detections)
+        cost = np.zeros(shape=(N, M))   # Cost matrix
+        for i in range(len(self.tracks)):
+            for j in range(len(detections)):
+                try:
+                    diff = self.tracks[i].prediction - detections[j]
+                    distance = np.sqrt(diff[0][0]*diff[0][0] +
+                                       diff[1][0]*diff[1][0])
+                    cost[i][j] = distance
+                except:
+                    pass
 
-        # Clean the dictionary by center points to remove IDS not used anymore
-        new_center_points = {}
-        for obj_bb_id in objects_bbs_ids:
-            _, _, _, _, object_id = obj_bb_id
-            center = self.center_points[object_id]
-            new_center_points[object_id] = center
+        # Let's average the squared ERROR
+        cost = (0.5) * cost
+        # Using Hungarian Algorithm assign the correct detected measurements
+        # to predicted tracks
+        assignment = []
+        for _ in range(N):
+            assignment.append(-1)
+        row_ind, col_ind = linear_sum_assignment(cost)
+        for i in range(len(row_ind)):
+            assignment[row_ind[i]] = col_ind[i]
 
-        # Update dictionary with IDs not used removed
-        self.center_points = new_center_points.copy()
-        return objects_bbs_ids
+        # Identify tracks with no assignment, if any
+        un_assigned_tracks = []
+        for i in range(len(assignment)):
+            if (assignment[i] != -1):
+                # check for cost distance threshold.
+                # If cost is very high then un_assign (delete) the track
+                if (cost[i][assignment[i]] > self.dist_thresh):
+                    assignment[i] = -1
+                    un_assigned_tracks.append(i)
+                pass
+            else:
+                self.tracks[i].skipped_frames += 1
 
+        # If tracks are not detected for long time, remove them
+        del_tracks = []
+        for i in range(len(self.tracks)):
+            if (self.tracks[i].skipped_frames > self.max_frames_to_skip):
+                del_tracks.append(i)
+        if len(del_tracks) > 0:  # only when skipped frame exceeds max
+            for id in del_tracks:
+                if id < len(self.tracks):
+                    del self.tracks[id]
+                    del assignment[id]
+                else:
+                    dprint("ERROR: id is greater than length of tracks")
 
+        # Now look for un_assigned detects
+        un_assigned_detects = []
+        for i in range(len(detections)):
+                if i not in assignment:
+                    un_assigned_detects.append(i)
 
+        # Start new tracks
+        if(len(un_assigned_detects) != 0):
+            for i in range(len(un_assigned_detects)):
+                track = Track(detections[un_assigned_detects[i]],
+                              self.trackIdCount)
+                self.trackIdCount += 1
+                self.tracks.append(track)
+
+        # Update KalmanFilter state, lastResults and tracks trace
+        for i in range(len(assignment)):
+            self.tracks[i].KF.predict()
+
+            if(assignment[i] != -1):
+                self.tracks[i].skipped_frames = 0
+                self.tracks[i].prediction = self.tracks[i].KF.correct(
+                                            detections[assignment[i]], 1)
+            else:
+                self.tracks[i].prediction = self.tracks[i].KF.correct(
+                                            np.array([[0], [0]]), 0)
+
+            if(len(self.tracks[i].trace) > self.max_trace_length):
+                for j in range(len(self.tracks[i].trace) -
+                               self.max_trace_length):
+                    del self.tracks[i].trace[j]
+
+            self.tracks[i].trace.append(self.tracks[i].prediction)
+            self.tracks[i].KF.lastResult = self.tracks[i].prediction
